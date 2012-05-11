6fd37652877bdc91bdc9f1c09a2cab7902c8a254
shavel <d.shavel@gmail.com>
2012-05-11 13:23:54 -0430

    Just first commit

---
+++ yabe/app/controllers/Application.java
@@ -0,0 +1,40 @@
+package controllers;
+
+import java.util.List;
+
+import models.Post;
+import play.Play;
+import play.mvc.Before;
+import play.mvc.Controller;
+
+
+public class Application extends Controller {
+    
+	@Before
+    static void addDefaults() {
+        renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
+        renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
+    }
+
+    public static void index() {
+    	Post frontPost = Post.find("order by postedAt desc").first();
+        List<Post> olderPosts = Post.find(
+            "order by postedAt desc"
+        ).from(1).fetch(10);
+        render(frontPost, olderPosts);
+    }
+    
+    
+    public static void show(Long id) {
+        Post post = Post.findById(id);
+        render(post);
+    }
+    
+    
+    public static void postComment(Long postId, String author, String content) {
+        Post post = Post.findById(postId);
+        post.addComment(author, content);
+        show(postId);
+    }
+
+}
